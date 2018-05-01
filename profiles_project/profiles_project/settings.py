fe0021816e72822cecfaed2c9b8201774ba5b6a1
Ejiro E. Obamrevwo <eeobamrevwo@sidmach.com>
2018-05-01 12:22:44 +0100

    added a custom user profile model, a manager and migrations

--- profiles_project/profiles_project/settings.py
+++ profiles_project/profiles_project/settings.py
@@ -84,12 +84,15 @@ WSGI_APPLICATION = 'profiles_project.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'profilesapi',
+            'USER': 'postgres',
+            'PASSWORD': 'root',
+            'HOST': '127.0.0.1',
+            'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
@@ -128,3 +131,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'profiles_api.UserProfile'

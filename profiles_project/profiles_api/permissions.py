5702e7dba05042b3aef7157a5d73b4df9e93c46f
Ejiro E. Obamrevwo <eeobamrevwo@sidmach.com>
2018-05-05 13:24:41 +0100

    Added improved permissions to user profile feed item

--- profiles_project/profiles_api/permissions.py
+++ profiles_project/profiles_api/permissions.py
@@ -10,4 +10,15 @@ class UpdateOwnProfile(permissions.BasePermission):
             return True
 
         return obj.id == request.user.id
-         
+
+class PostOwnStatus(permissions.BasePermission):
+    """Allow users to update their own status"""
+
+    def has_object_permissions(self,request,view,obj):
+        """checks the user is trying to update their own status"""
+
+        if request.methos in permissions.SAFE_METHODS:
+            return True
+        
+        return obj.user_profile.id == request.user.id
+        """checks if the users status id matches the user that is logged in"""
--- profiles_project/profiles_api/urls.py
+++ profiles_project/profiles_api/urls.py
@@ -8,6 +8,7 @@ from . import views
 router = DefaultRouter()
 router.register('profile', views.UserProfileViewSet)
 router.register('login',views.LoginViewSet, base_name='login')
+router.register('feed',views.UserProfileFeedItemViewset)
 urlpatterns = [
     url(r'^', include(router.urls)),
     # url(r'^token/', auth_views.obtain_auth_token),

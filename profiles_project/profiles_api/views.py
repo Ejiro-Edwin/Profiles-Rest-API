b988bb5c713542c27705ce669f2b0553df40cee9
Ejiro E. Obamrevwo <eeobamrevwo@sidmach.com>
2018-05-05 06:15:43 +0100

    Added model for storing user profile feed/status post Item, created migrations and added it to django admin

--- profiles_project/profiles_api/views.py
+++ profiles_project/profiles_api/views.py
@@ -7,7 +7,8 @@ from rest_framework import viewsets
 from . import serializers, models, permissions
 from rest_framework.authentication import TokenAuthentication
 from rest_framework  import filters
-
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from rest_framework.authtoken.views import ObtainAuthToken
 
 class UserProfileViewSet(viewsets.ModelViewSet):
     """Handles creating, reading and updating profiles."""
@@ -18,3 +19,12 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.UpdateOwnProfile,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name','email',)
+
+class LoginViewSet(viewsets.ViewSet):
+    """Checks email and password and returns an auth token"""
+    serializer_class = AuthTokenSerializer
+
+    def create(self, request):
+        """Use the ObtainAuthToken APIView to validate and create a token."""
+
+        return ObtainAuthToken().post(request)
